b1768697a743808a223f67807a799c129c7abbf5
juancoquet <juancoquet@gmail.com>
2023-03-12 20:44:15 +0000

    solve day 4.2

--- 2022/day4/Program.cs
+++ 2022/day4/Program.cs
@@ -1,6 +1,23 @@
 ﻿class Program
 {
-    private static int FindContainedRanges(IEnumerable<string> input)
+    private static int FindContainedRanges(IEnumerable<(int a0, int a1, int b0, int b1)> tuples)
+    {
+        var fullyContained = tuples.Where(
+            x => ((x.a0 >= x.b0 && x.a1 <= x.b1) || (x.a0 <= x.b0 && x.a1 >= x.b1))
+        );
+
+        return fullyContained.Count();
+    }
+
+    private static int FindOverlappingRanges(IEnumerable<(int a0, int a1, int b0, int b1)> tuples)
+    {
+        var overlaps = tuples.Where(
+            x => ((x.a0 >= x.b0 && x.a0 <= x.b1) || (x.b0 >= x.a0 && x.b0 <= x.a1))
+        );
+        return overlaps.Count();
+    }
+
+    private static IEnumerable<(int, int, int, int)> PrepareData(IEnumerable<string> input)
     {
         var tuples = input
             .Select(
@@ -11,12 +28,7 @@
                         .ToList()
             )
             .Select(ab => (a0: ab[0], a1: ab[1], b0: ab[2], b1: ab[3]));
-
-        var fullyContained = tuples.Where(
-            x => ((x.a0 >= x.b0 && x.a1 <= x.b1) || (x.a0 <= x.b0 && x.a1 >= x.b1))
-        );
-
-        return fullyContained.Count();
+        return tuples;
     }
 
     private static IEnumerable<string> GetInput()
@@ -27,7 +39,12 @@
     public static void Main(string[] args)
     {
         var input = GetInput();
-        var fullyContained = FindContainedRanges(input); // challenge 1 answer
+        var tuples = PrepareData(input);
+
+        var fullyContained = FindContainedRanges(tuples); // challenge 1 answer
         Console.WriteLine(fullyContained);
+
+        var overlaps = FindOverlappingRanges(tuples); // challenge 2 answer
+        Console.WriteLine(overlaps);
     }
 }
